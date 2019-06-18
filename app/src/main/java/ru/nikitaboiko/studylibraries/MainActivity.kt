5e1597ef846611b77e8b3dd3592b853777936935
Nikita Boiko <genertyniki@gmail.com>
2019-06-18 16:09:26 +0300

    add lesson theme

--- app/src/main/java/ru/nikitaboiko/studylibraries/MainActivity.kt
+++ app/src/main/java/ru/nikitaboiko/studylibraries/MainActivity.kt
@@ -2,31 +2,48 @@ package ru.nikitaboiko.studylibraries
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.arellomobile.mvp.presenter.ProvidePresenter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : MvpAppCompatActivity(), MainView {
+class MainActivity : MvpAppCompatActivity(), MainView, View.OnClickListener {
+    lateinit var buttonOne: Button
+    lateinit var buttonTwo: Button
+    lateinit var buttonThree: Button
+
+    @InjectPresenter
+    internal lateinit var presenter: MainPresenter
+
+
+    @ProvidePresenter
+    fun providePresenter(): MainPresenter {
+        return MainPresenter(Model())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonOne = activity_main_button_one
-        val buttonTwo = activity_main_button_two
-        val buttonThree = activity_main_button_three
+        buttonOne = activity_main_button_one
+        buttonTwo = activity_main_button_two
+        buttonThree = activity_main_button_three
+        buttonOne.setOnClickListener(this)
+        buttonTwo.setOnClickListener(this)
+        buttonThree.setOnClickListener(this)
+    }
 
-        buttonOne.setOnClickListener { v: View ->
-
-
-        }
-        buttonTwo.setOnClickListener {
-
-        }
-        buttonThree.setOnClickListener {
-
-        }
+    override fun onClick(v: View?) {
+        if (v == null) return
+        presenter.counterClick(v.id)
     }
 
     override fun setButtonText(btnIndex: Int, value: Int) {
-        1
+        val currentText = "Количество = $value"
+        when (btnIndex) {
+            0 -> buttonOne.text = currentText
+            1 -> buttonTwo.text = currentText
+            2 -> buttonThree.text = currentText
+        }
     }
 }
--- app/src/main/java/ru/nikitaboiko/studylibraries/MainPresenter.kt
+++ app/src/main/java/ru/nikitaboiko/studylibraries/MainPresenter.kt
@@ -14,4 +14,12 @@ class MainPresenter(val model: Model) : MvpPresenter<MainView>() {
         model.setAt(index, model.getAt(index) + 1)
         return model.getAt(index)
     }
+
+    fun counterClick(id: Int) {
+        when (id) {
+            R.id.activity_main_button_one -> viewState.setButtonText(0, calcValue(0))
+            R.id.activity_main_button_two -> viewState.setButtonText(1, calcValue(1))
+            R.id.activity_main_button_three -> viewState.setButtonText(2, calcValue(2))
+        }
+    }
 }

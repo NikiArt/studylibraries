89622aa1a93f4e2607807232504e0583c018c2ad
Nikita Boiko <genertyniki@gmail.com>
2019-06-24 15:20:35 +0300

    add event bus realisation

--- app/src/main/java/ru/nikitaboiko/studylibraries/App.kt
+++ app/src/main/java/ru/nikitaboiko/studylibraries/App.kt
@@ -1,6 +1,20 @@
 package ru.nikitaboiko.studylibraries
 
 import android.app.Application
+import io.reactivex.subjects.PublishSubject
 
 class App : Application() {
+    val publishSubject = PublishSubject.create<Int>()
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+
+        fun instance(): App {
+            return instance
+        }
+    }
 }
--- app/src/main/java/ru/nikitaboiko/studylibraries/task02/Main2Activity.kt
+++ app/src/main/java/ru/nikitaboiko/studylibraries/task02/Main2Activity.kt
@@ -1,20 +1,50 @@
 package ru.nikitaboiko.studylibraries.task02
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main2.*
+import ru.nikitaboiko.studylibraries.App
 import ru.nikitaboiko.studylibraries.R
 
 class Main2Activity : AppCompatActivity() {
+    lateinit var textLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val textLabel = activity_main_two_text
+        textLabel = activity_main_two_text
         val editText = activity_main_two_edit
+        val button = activity_main_two_button
+
+        button.setOnClickListener {
+            App.instance().publishSubject.onNext((Math.random() * 1000).toInt())
+        }
+
         editText.textChanges().subscribe() {
             textLabel.text = it.toString()
+        }
+
+        App.instance().publishSubject.subscribe(setLabelText())
+    }
+
+    fun setLabelText(): Observer<Int> {
+        return object : Observer<Int> {
+            override fun onComplete() {
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onError(e: Throwable) {
+            }
+
+            override fun onNext(value: Int) {
+                textLabel.text = value.toString()
+            }
         }
     }
 }
